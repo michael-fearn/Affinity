4319381cba35dafb9fc59f50d65728cd8f132e5b
Michael Fearn <fearn.michael@gmail.com>
2018-07-26 12:23:53 -0600

    more adjustments

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ const io = socket(server)
 
 require('dotenv').config()
 
+app.use(express.static( __dirname+'/../build'))
+
 server.listen(process.env.SERVER_PORT, () => console.log(`listening on port ${process.env.SERVER_PORT}`))
 
 

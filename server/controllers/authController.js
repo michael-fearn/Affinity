9181c5abe92293bc82f4b7eaf0fbcecfec0356a0
Michael Fearn <fearn.michael@gmail.com>
2018-07-26 15:28:16 -0600

    adjustments

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -14,7 +14,7 @@ module.exports = async (req, res) => {
         client_secret: CLIENT_SECRET,
         code: req.query.code,
         grant_type: 'authorization_code',
-        redirect_uri: `http://${req.headers.host}/auth/callback`
+        redirect_uri: `https://${req.headers.host}/auth/callback`
     };
     try {
         console.log(11111)
